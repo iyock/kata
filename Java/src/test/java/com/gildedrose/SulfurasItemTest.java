6d73cd56a54ec416e0fd8de2be819914b1781aa4
Ibrahima Yock <Wolters Kluwer>
2020-08-31 19:03:09 +0200

    Improvement

--- Java/src/test/java/com/gildedrose/SulfurasItemTest.java
+++ Java/src/test/java/com/gildedrose/SulfurasItemTest.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ConjuredItemTest {
+public class SulfurasItemTest {
+
     @Test
-    void conjuredItemTest_Before_Sellin_Date(){
+    void sulfurasItemTest_Before_Sellin_Date(){
         final Item[] items = createItem();
         final GildedRose app = new GildedRose(items);
         final int days = 4;
@@ -18,13 +19,13 @@ public class ConjuredItemTest {
             app.updateQuality(remainingDays);
             --remainingDays;
         }
-        assertEquals("Conjured Item", app.items[0].name);
-        assertEquals(2, app.items[0].sellIn);
-        assertEquals(6, app.items[0].quality);
+        assertEquals("Sulfuras Item", app.items[0].name);
+        assertEquals(6, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
     }
 
     @Test
-    void conjuredTest_On_Sellin_Date(){
+    void sulfurasItemTest_On_Sellin_Date(){
         final Item[] items = createItem();
         final GildedRose app = new GildedRose(items);
         final int days = 6;
@@ -36,13 +37,13 @@ public class ConjuredItemTest {
             app.updateQuality(remainingDays);
             --remainingDays;
         }
-        assertEquals("Conjured Item", app.items[0].name);
-        assertEquals(0, app.items[0].sellIn);
-        assertEquals(2, app.items[0].quality);
+        assertEquals("Sulfuras Item", app.items[0].name);
+        assertEquals(6, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
     }
 
     @Test
-    void conjuredItemTest_After_Sellin_Date(){
+    void sulfurasItemTest_After_Sellin_Date(){
         final Item[] items = createItem();
         final GildedRose app = new GildedRose(items);
         final int days = 7;
@@ -54,13 +55,14 @@ public class ConjuredItemTest {
             app.updateQuality(remainingDays);
             --remainingDays;
         }
-        assertEquals("Conjured Item", app.items[0].name);
-        assertEquals(-1, app.items[0].sellIn);
-        assertEquals(0, app.items[0].quality);
+        assertEquals("Sulfuras Item", app.items[0].name);
+        assertEquals(6, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
     }
 
+
     private Item[] createItem (){
-        return new Item[]{new Item("Conjured Item", 6, 14) };
+        return new Item[]{new Item("Sulfuras Item", 6, 80) };
     }
 
 }
